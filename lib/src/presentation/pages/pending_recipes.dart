c70307c012db2449d6ccede89683e2593cce9627
Wag 31 <rupcyes@mail.ru>
2024-04-24 12:37:21 +0300

    возможность оформить черновик

--- lib/src/presentation/pages/pending_recipes.dart
+++ lib/src/presentation/pages/pending_recipes.dart
@@ -1,4 +1,5 @@
 import 'package:accounting_chemical_reagents/src/domain/model/reagent.dart';
+import 'package:accounting_chemical_reagents/src/domain/model/reagents_recipe.dart';
 import 'package:accounting_chemical_reagents/src/domain/model/recipe.dart';
 import 'package:accounting_chemical_reagents/src/domain/model/warehouse.dart';
 import 'package:accounting_chemical_reagents/src/domain/repository/reagent_repository.dart';
@@ -18,6 +19,7 @@ class PendingRecipes extends StatefulWidget {
 class _PendingRecipesStateState extends State<PendingRecipes> {
   late Future<List<RecipeModel>> _fetchEnoughRecipesFuture;
   late Future<List<RecipeModel>> _fetchNoEnoughRecipesFuture;
+  bool isOrder = true;
 
   @override
   void initState() {
@@ -241,7 +243,7 @@ class _PendingRecipesStateState extends State<PendingRecipes> {
                         '№${recipe.id}',
                         style: const TextStyle(fontSize: 22),
                       ),
-                      children: [_showRecipeInfo(recipe)],
+                      children: [_showNoEnoughRecipeInfo(recipe)],
                     );
                   },
                 );
@@ -251,5 +253,160 @@ class _PendingRecipesStateState extends State<PendingRecipes> {
         ))
       ],
     );
+  }
+
+  Widget _showNoEnoughRecipeInfo(RecipeModel recipe) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        _buildNoEnoughRecipeReagentsInfo(recipe.id!),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _buildOrderRecipeButton(recipe),
+              _buildDeleteRecipeButton(recipe)
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildNoEnoughRecipeReagentsInfo(int recipeId) {
+    return FutureBuilder(
+      future: _getNoEnoughRecipeReagentsInfo(recipeId),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text('Ошибка: ${snapshot.error}');
+        } else {
+          return Text('${snapshot.data}', style: const TextStyle(fontSize: 24));
+        }
+      },
+    );
+  }
+
+  Future<String> _getNoEnoughRecipeReagentsInfo(int recipeId) async {
+    List<Map<String, dynamic>> reagents = await RecipeReagentRepository().getReagentsForRecipe(recipeId);
+    String reagentsInfo = '';
+
+    for (int i = 0; i < reagents.length; i++) {
+      int reagentId = reagents[i]['reagent_id'] as int;
+      int quantity = reagents[i]['quantity'] as int;
+
+      Reagent reagent = await ReagentRepository().getReagentById(reagentId);
+      WarehouseModel? warehouse = await WarehouseRepository().getElementByReagentId(reagentId);
+
+      if (warehouse != null) {
+        reagentsInfo += '${reagent.name} • $quantity/${warehouse.quantity}';
+      } else {
+        reagentsInfo += '${reagent.name} • $quantity/0';
+      }
+
+      if (i < reagents.length - 1) {
+        reagentsInfo += '\n';
+      }
+    }
+    return reagentsInfo;
+  }
+
+  Widget _buildOrderRecipeButton(RecipeModel recipe) {
+    return ElevatedButton(
+        onPressed: () {
+          _orderRecipe(recipe);
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blue[300],
+        ),
+        child: const Text(
+          'Оформить',
+          style: TextStyle(color: Colors.white, fontSize: 22),
+        ));
+  }
+
+  Future<void> _orderRecipe(RecipeModel recipe) async {
+    List<Map<String, dynamic>> reagents = await RecipeReagentRepository().getReagentsForRecipe(recipe.id!);
+
+    isOrder = true;
+    await _checkEnoughReagents(reagents);
+
+    if (!isOrder) {
+      return;
+    }
+
+    RecipeModel updateRecipe = RecipeModel(id: recipe.id, isAccepted: false, isEnough: true);
+    await RecipeRepository().updateRecipe(updateRecipe);
+
+    await _warehouseManagement(updateRecipe, recipe.id!);
+
+    _refreshEnoughRecipesData();
+    _refreshNoEnoughRecipesData();
+  }
+
+  Future<void> _checkEnoughReagents(List<Map<String, dynamic>> reagents) async {
+    List<ReagentsRecipe> reagentsRecipe = [];
+
+    for (int i = 0; i < reagents.length; i++) {
+      int reagentId = reagents[i]['reagent_id'] as int;
+      int quantity = reagents[i]['quantity'] as int;
+
+      ReagentsRecipe reagentRecipe = ReagentsRecipe(reagentId: reagentId, quantity: quantity);
+      reagentsRecipe.add(reagentRecipe);
+    }
+
+    for (int i = 0; i < reagentsRecipe.length; i++) {
+      WarehouseModel? warehouseModel = await WarehouseRepository().getElementByReagentId(reagentsRecipe[i].reagentId);
+      if (warehouseModel == null || warehouseModel.quantity < reagentsRecipe[i].quantity) {
+        await _showConfirmationDialog();
+        return;
+      }
+    }
+  }
+
+  Future<void> _showConfirmationDialog() async {
+    await showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Предупреждение'),
+            content: const Text('В вашем списке есть элемент с превышающим количеством'),
+            actions: [
+              Row(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    isOrder = false;
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text(
+                    'Продолжить',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ])
+            ],
+          );
+        });
+  }
+
+  Future<void> _warehouseManagement(RecipeModel recipe, int recipeId) async {
+    if (recipe.isEnough) {
+      List<Map<String, dynamic>> reagents = await RecipeReagentRepository().getReagentsForRecipe(recipeId);
+
+      for (int i = 0; i < reagents.length; i++) {
+        int reagentId = reagents[i]['reagent_id'] as int;
+        int quantity = reagents[i]['quantity'] as int;
+
+        WarehouseModel? warehouse = await WarehouseRepository().getElementByReagentId(reagentId);
+        WarehouseModel newWarehouse = WarehouseModel(
+            id: warehouse!.id,
+            reagentId: warehouse.reagentId,
+            quantity: warehouse.quantity - quantity);
+        await WarehouseRepository().updateElement(newWarehouse);
+      }
+    }
   }
 }
